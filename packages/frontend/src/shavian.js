caf942af0a3270fe4be98b3de632a5ca0c407ba1
Charles Sampson <csampson@qualtrics.com>
2024-06-12 15:59:58 -0600

    no button fade on mobile

--- packages/frontend/src/shavian.js
+++ packages/frontend/src/shavian.js
@@ -1,338 +1,338 @@
 export const SHAVIAN_LETTERS = [
-    {
-      "letter": "𐑐",
-      "name": "pin",
-      "sound": "(p)eep",
-      "type": "Tall",
-      "code": "50"
-    },
-    {
-      "letter": "𐑚",
-      "name": "bell",
-      "sound": "(b)ib",
-      "type": "Deep",
-      "code": "5A"
-    },
-    {
-      "letter": "𐑑",
-      "name": "tan",
-      "sound": "(t)ot",
-      "type": "Tall",
-      "code": "51"
-    },
-    {
-      "letter": "𐑛",
-      "name": "done",
-      "sound": "(d)ead",
-      "type": "Deep",
-      "code": "5B"
-    },
-    {
-      "letter": "𐑒",
-      "name": "key",
-      "sound": "(k)ick",
-      "type": "Tall",
-      "code": "52"
-    },
-    {
-      "letter": "𐑜",
-      "name": "gill",
-      "sound": "(g)ag",
-      "type": "Deep",
-      "code": "5C"
-    },
-    {
-      "letter": "𐑓",
-      "name": "fay",
-      "sound": "(f)ee",
-      "type": "Tall",
-      "code": "53"
-    },
-    {
-      "letter": "𐑝",
-      "name": "vie",
-      "sound": "(v)ow",
-      "type": "Deep",
-      "code": "5D"
-    },
-    {
-      "letter": "𐑔",
-      "name": "thorn",
-      "sound": "(th)igh",
-      "type": "Tall",
-      "code": "54"
-    },
-    {
-      "letter": "𐑞",
-      "name": "thou",
-      "sound": "(th)ey",
-      "type": "Deep",
-      "code": "5E"
-    },
-    {
-      "letter": "𐑕",
-      "name": "see",
-      "sound": "(s)o",
-      "type": "Tall",
-      "code": "55"
-    },
-    {
-      "letter": "𐑟",
-      "name": "zoo",
-      "sound": "(z)oo",
-      "type": "Deep",
-      "code": "5F"
-    },
-    {
-      "letter": "𐑖",
-      "name": "shy",
-      "sound": "(s)ure",
-      "type": "Tall",
-      "code": "56"
-    },
-    {
-      "letter": "𐑠",
-      "name": "zho",
-      "sound": "mea(s)ure",
-      "type": "Deep",
-      "code": "60"
-    },
-    {
-      "letter": "𐑗",
-      "name": "cha",
-      "sound": "(ch)urch",
-      "type": "Tall",
-      "code": "57"
-    },
-    {
-      "letter": "𐑡",
-      "name": "joy",
-      "sound": "(j)udge",
-      "type": "Deep",
-      "code": "61"
-    },
-    {
-      "letter": "𐑘",
-      "name": "yen",
-      "sound": "(y)ea",
-      "type": "Tall",
-      "code": "58"
-    },
-    {
-      "letter": "𐑢",
-      "name": "way",
-      "sound": "(w)oe",
-      "type": "Deep",
-      "code": "62"
-    },
-    {
-      "letter": "𐑙",
-      "name": "song",
-      "sound": "hu(ng)",
-      "type": "Tall",
-      "code": "59"
-    },
-    {
-      "letter": "𐑣",
-      "name": "who",
-      "sound": "(h)a-ha",
-      "type": "Deep",
-      "code": "63"
-    },
-    {
-      "letter": "𐑤",
-      "name": "lamb",
-      "sound": "(l)oll",
-      "type": "Short",
-      "code": "64"
-    },
-    {
-      "letter": "𐑮",
-      "name": "roe",
-      "sound": "(r)oar",
-      "type": "Short",
-      "code": "6E"
-    },
-    {
-      "letter": "𐑥",
-      "name": "me",
-      "sound": "(m)ime",
-      "type": "Short",
-      "code": "65"
-    },
-    {
-      "letter": "𐑯",
-      "name": "now",
-      "sound": "(n)un",
-      "type": "Short",
-      "code": "6F"
-    },
-    {
-      "letter": "𐑦",
-      "name": "in",
-      "sound": "(i)f",
-      "type": "Short",
-      "code": "66"
-    },
-    {
-      "letter": "𐑰",
-      "name": "eve",
-      "sound": "(ea)t",
-      "type": "Short",
-      "code": "70"
-    },
-    {
-      "letter": "𐑧",
-      "name": "edge",
-      "sound": "(e)gg",
-      "type": "Short",
-      "code": "67"
-    },
-    {
-      "letter": "𐑱",
-      "name": "aim",
-      "sound": "(a)ge",
-      "type": "Short",
-      "code": "71"
-    },
-    {
-      "letter": "𐑨",
-      "name": "ash",
-      "sound": "(a)sh",
-      "type": "Short",
-      "code": "68"
-    },
-    {
-      "letter": "𐑲",
-      "name": "eyes",
-      "sound": "(i)ce",
-      "type": "Short",
-      "code": "72"
-    },
-    {
-      "letter": "𐑩",
-      "name": "ago",
-      "sound": "(a)do",
-      "type": "Short",
-      "code": "69"
-    },
-    {
-      "letter": "𐑳",
-      "name": "up",
-      "sound": "(u)p",
-      "type": "Short",
-      "code": "73"
-    },
-    {
-      "letter": "𐑪",
-      "name": "on",
-      "sound": "(o)n",
-      "type": "Short",
-      "code": "6A"
-    },
-    {
-      "letter": "𐑴",
-      "name": "oath",
-      "sound": "(oa)k",
-      "type": "Short",
-      "code": "74"
-    },
-    {
-      "letter": "𐑫",
-      "name": "oomph",
-      "sound": "w(oo)l",
-      "type": "Short",
-      "code": "6B"
-    },
-    {
-      "letter": "𐑵",
-      "name": "ooze",
-      "sound": "(oo)ze",
-      "type": "Short",
-      "code": "75"
-    },
-    {
-      "letter": "𐑬",
-      "name": "ounce",
-      "sound": "(ou)t",
-      "type": "Short",
-      "code": "6C"
-    },
-    {
-      "letter": "𐑶",
-      "name": "oil",
-      "sound": "(oi)l",
-      "type": "Short",
-      "code": "76"
-    },
-    {
-      "letter": "𐑭",
-      "name": "alms",
-      "sound": "(ah)",
-      "type": "Short",
-      "code": "6D"
-    },
-    {
-      "letter": "𐑷",
-      "name": "ought",
-      "sound": "(awe)",
-      "type": "Short",
-      "code": "77"
-    },
-    {
-      "letter": "𐑸",
-      "name": "aria",
-      "sound": "(ar)e",
-      "type": "Compound",
-      "code": "78"
-    },
-    {
-      "letter": "𐑹",
-      "name": "aura",
-      "sound": "(or)",
-      "type": "Compound",
-      "code": "79"
-    },
-    {
-      "letter": "𐑺",
-      "name": "aero",
-      "sound": "(air)",
-      "type": "Compound",
-      "code": "7A"
-    },
-    {
-      "letter": "𐑻",
-      "name": "early",
-      "sound": "(err)",
-      "type": "Compound",
-      "code": "7B"
-    },
-    {
-      "letter": "𐑼",
-      "name": "array",
-      "sound": "(arr)ay",
-      "type": "Compound",
-      "code": "7C"
-    },
-    {
-      "letter": "𐑽",
-      "name": "eerie",
-      "sound": "(ear)",
-      "type": "Compound",
-      "code": "7D"
-    },
-    {
-      "letter": "𐑾",
-      "name": "ian",
-      "sound": "(ia)n",
-      "type": "Compound",
-      "code": "7E"
-    },
-    {
-      "letter": "𐑿",
-      "name": "yule",
-      "sound": "(yew)",
-      "type": "Compound",
-      "code": "7F"
-    }
-   ]
+  {
+    "letter": "𐑐",
+    "name": "pin",
+    "sound": "(p)eep",
+    "type": "Tall",
+    "code": "50"
+  },
+  {
+    "letter": "𐑚",
+    "name": "bell",
+    "sound": "(b)ib",
+    "type": "Deep",
+    "code": "5A"
+  },
+  {
+    "letter": "𐑑",
+    "name": "tan",
+    "sound": "(t)ot",
+    "type": "Tall",
+    "code": "51"
+  },
+  {
+    "letter": "𐑛",
+    "name": "done",
+    "sound": "(d)ead",
+    "type": "Deep",
+    "code": "5B"
+  },
+  {
+    "letter": "𐑒",
+    "name": "key",
+    "sound": "(k)ick",
+    "type": "Tall",
+    "code": "52"
+  },
+  {
+    "letter": "𐑜",
+    "name": "gill",
+    "sound": "(g)ag",
+    "type": "Deep",
+    "code": "5C"
+  },
+  {
+    "letter": "𐑓",
+    "name": "fay",
+    "sound": "(f)ee",
+    "type": "Tall",
+    "code": "53"
+  },
+  {
+    "letter": "𐑝",
+    "name": "vie",
+    "sound": "(v)ow",
+    "type": "Deep",
+    "code": "5D"
+  },
+  {
+    "letter": "𐑔",
+    "name": "thorn",
+    "sound": "(th)igh",
+    "type": "Tall",
+    "code": "54"
+  },
+  {
+    "letter": "𐑞",
+    "name": "thou",
+    "sound": "(th)ey",
+    "type": "Deep",
+    "code": "5E"
+  },
+  {
+    "letter": "𐑕",
+    "name": "see",
+    "sound": "(s)o",
+    "type": "Tall",
+    "code": "55"
+  },
+  {
+    "letter": "𐑟",
+    "name": "zoo",
+    "sound": "(z)oo",
+    "type": "Deep",
+    "code": "5F"
+  },
+  {
+    "letter": "𐑖",
+    "name": "shy",
+    "sound": "(s)ure",
+    "type": "Tall",
+    "code": "56"
+  },
+  {
+    "letter": "𐑠",
+    "name": "zho",
+    "sound": "mea(s)ure",
+    "type": "Deep",
+    "code": "60"
+  },
+  {
+    "letter": "𐑗",
+    "name": "cha",
+    "sound": "(ch)urch",
+    "type": "Tall",
+    "code": "57"
+  },
+  {
+    "letter": "𐑡",
+    "name": "joy",
+    "sound": "(j)udge",
+    "type": "Deep",
+    "code": "61"
+  },
+  {
+    "letter": "𐑘",
+    "name": "yen",
+    "sound": "(y)ea",
+    "type": "Tall",
+    "code": "58"
+  },
+  {
+    "letter": "𐑢",
+    "name": "way",
+    "sound": "(w)oe",
+    "type": "Deep",
+    "code": "62"
+  },
+  {
+    "letter": "𐑙",
+    "name": "song",
+    "sound": "hu(ng)",
+    "type": "Tall",
+    "code": "59"
+  },
+  {
+    "letter": "𐑣",
+    "name": "who",
+    "sound": "(h)a-ha",
+    "type": "Deep",
+    "code": "63"
+  },
+  {
+    "letter": "𐑤",
+    "name": "lamb",
+    "sound": "(l)oll",
+    "type": "Short",
+    "code": "64"
+  },
+  {
+    "letter": "𐑮",
+    "name": "roe",
+    "sound": "(r)oar",
+    "type": "Short",
+    "code": "6E"
+  },
+  {
+    "letter": "𐑥",
+    "name": "me",
+    "sound": "(m)ime",
+    "type": "Short",
+    "code": "65"
+  },
+  {
+    "letter": "𐑯",
+    "name": "now",
+    "sound": "(n)un",
+    "type": "Short",
+    "code": "6F"
+  },
+  {
+    "letter": "𐑦",
+    "name": "in",
+    "sound": "(i)f",
+    "type": "Short",
+    "code": "66"
+  },
+  {
+    "letter": "𐑰",
+    "name": "eve",
+    "sound": "(ea)t",
+    "type": "Short",
+    "code": "70"
+  },
+  {
+    "letter": "𐑧",
+    "name": "edge",
+    "sound": "(e)gg",
+    "type": "Short",
+    "code": "67"
+  },
+  {
+    "letter": "𐑱",
+    "name": "aim",
+    "sound": "(a)ge",
+    "type": "Short",
+    "code": "71"
+  },
+  {
+    "letter": "𐑨",
+    "name": "ash",
+    "sound": "(a)sh",
+    "type": "Short",
+    "code": "68"
+  },
+  {
+    "letter": "𐑲",
+    "name": "eyes",
+    "sound": "(i)ce",
+    "type": "Short",
+    "code": "72"
+  },
+  {
+    "letter": "𐑩",
+    "name": "ago",
+    "sound": "(a)do",
+    "type": "Short",
+    "code": "69"
+  },
+  {
+    "letter": "𐑳",
+    "name": "up",
+    "sound": "(u)p",
+    "type": "Short",
+    "code": "73"
+  },
+  {
+    "letter": "𐑪",
+    "name": "on",
+    "sound": "(o)n",
+    "type": "Short",
+    "code": "6A"
+  },
+  {
+    "letter": "𐑴",
+    "name": "oath",
+    "sound": "(oa)k",
+    "type": "Short",
+    "code": "74"
+  },
+  {
+    "letter": "𐑫",
+    "name": "oomph",
+    "sound": "w(oo)l",
+    "type": "Short",
+    "code": "6B"
+  },
+  {
+    "letter": "𐑵",
+    "name": "ooze",
+    "sound": "(oo)ze",
+    "type": "Short",
+    "code": "75"
+  },
+  {
+    "letter": "𐑬",
+    "name": "ounce",
+    "sound": "(ou)t",
+    "type": "Short",
+    "code": "6C"
+  },
+  {
+    "letter": "𐑶",
+    "name": "oil",
+    "sound": "(oi)l",
+    "type": "Short",
+    "code": "76"
+  },
+  {
+    "letter": "𐑭",
+    "name": "alms",
+    "sound": "(ah)",
+    "type": "Short",
+    "code": "6D"
+  },
+  {
+    "letter": "𐑷",
+    "name": "ought",
+    "sound": "(awe)",
+    "type": "Short",
+    "code": "77"
+  },
+  {
+    "letter": "𐑸",
+    "name": "aria",
+    "sound": "(ar)e",
+    "type": "Compound",
+    "code": "78"
+  },
+  {
+    "letter": "𐑹",
+    "name": "aura",
+    "sound": "(or)",
+    "type": "Compound",
+    "code": "79"
+  },
+  {
+    "letter": "𐑺",
+    "name": "aero",
+    "sound": "(air)",
+    "type": "Compound",
+    "code": "7A"
+  },
+  {
+    "letter": "𐑻",
+    "name": "early",
+    "sound": "(err)",
+    "type": "Compound",
+    "code": "7B"
+  },
+  {
+    "letter": "𐑼",
+    "name": "array",
+    "sound": "(arr)ay",
+    "type": "Compound",
+    "code": "7C"
+  },
+  {
+    "letter": "𐑽",
+    "name": "eerie",
+    "sound": "(ear)",
+    "type": "Compound",
+    "code": "7D"
+  },
+  {
+    "letter": "𐑾",
+    "name": "ian",
+    "sound": "(ia)n",
+    "type": "Compound",
+    "code": "7E"
+  },
+  {
+    "letter": "𐑿",
+    "name": "yule",
+    "sound": "(yew)",
+    "type": "Compound",
+    "code": "7F"
+  }
+]
